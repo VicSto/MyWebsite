0fb669017bc2f37d67c0ffdcf2e9c79abaa34084
Victor S <vstolle@uci.edu>
2020-09-09 23:23:41 -0700

    Added Javascript file for the homepage, and added fading transitions between the content on the homepage. Currently the fading only works when scrolling on desktop web browsers.
            modified:   index.js
            modified:   public/css/main.css
            new file:   public/js/home.js
            modified:   views/home.handlebars
            modified:   views/layouts/main.handlebars

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-
 const app = express();
 
 // Handlebars Middleware
@@ -12,24 +11,24 @@ app.set('view engine', 'handlebars');
 // Retrieve public Folder
 app.use(express.static(__dirname + '/public'));
 
-// Index Route
+// Index Routes
 app.get('/', (req, res) => {
     res.render('home', {
     });
 }); 
-
 app.get('/about', (req, res) => {
     res.render('about', {
     });
 });
-
 app.get('/projects', (req,res) =>{
     res.render('projects', {
     });
 });
 
+// PORT USED FOR WEBHOSTING
 const port = process.env.PORT || 5000;
 
+// LAUNCH WEBSITE
 app.listen(port, () => {
     console.log(`Server Started on Port ${port}`);
 });
---
+++ public/js/home.js
@@ -0,0 +1,137 @@
+const pages = {
+    HOME : 0,
+    ABOUT_PAGE : 1,
+    PROJECTS_PAGE : 2
+}
+
+const scrollDir = {
+    UP: 0,
+    DOWN: 1
+}
+
+var is_fading = false;
+var fade_timeout = 750;
+
+let currIndex = pages.HOME;
+
+let home_navlinks = document.getElementById("home_navigation_list")
+let about_page = document.getElementById("home_about_page")
+let projects_page = document.getElementById("home_projects_page")
+
+$(document).ready(function () {
+
+    // fade in initial element
+    throttled_fade_in("#home_navigation_list");
+
+    // bind our scroll event logic to all browser types
+    $('html').bind('mousewheel DOMMouseScroll touchmove', function (e) {
+        // console.log(e.type)
+        var delta = 0;
+        if (e.type === "mousewheel" || e.type === "DOMMouseScroll")
+        {
+            var delta = (e.originalEvent.wheelDelta || -e.originalEvent.detail);
+        }
+        else if (e.type === "touchmove"){
+            // placeholder, need to add logic for touchscreen scrolling
+            
+        }
+        if (is_fading === false){
+            toggle_element_view((delta < 0) ? scrollDir.DOWN : scrollDir.UP);
+        }
+    });
+});
+
+function toggle_element_view(scroll_dir){
+    is_fading = true;
+    switch(currIndex)
+    {
+        case pages.HOME:
+            if (scroll_dir === scrollDir.DOWN)
+            {
+                throttled_fade_out("#home_navigation_list");
+                setTimeout(
+                    function () {
+                        throttled_fade_in("#home_about_page");
+                    }, fade_timeout
+                );
+                currIndex = pages.ABOUT_PAGE;
+            }
+            break;
+        
+        case pages.ABOUT_PAGE:
+            if (scroll_dir === scrollDir.UP)
+            {
+                throttled_fade_out("#home_about_page");
+                setTimeout(
+                    function () {
+                        throttled_fade_in("#home_navigation_list");
+                    }, fade_timeout
+                );
+                currIndex = pages.HOME;
+            }
+            else if (scroll_dir === scrollDir.DOWN)
+            {
+                throttled_fade_out("#home_about_page");
+                setTimeout(
+                    function () {
+                        throttled_fade_in("#home_projects_page");
+                    }, fade_timeout
+                );
+                currIndex = pages.PROJECTS_PAGE;
+            }
+            break;
+
+        case pages.PROJECTS_PAGE:
+            if (scroll_dir === scrollDir.UP)
+            {
+                throttled_fade_out("#home_projects_page");
+                setTimeout(
+                    function () {
+                        throttled_fade_in("#home_about_page");
+                    }, fade_timeout
+                );
+                currIndex = pages.ABOUT_PAGE;
+            }
+            break;
+        
+    }
+    setTimeout(
+        function () {
+            is_fading = false;
+            return;
+        }, fade_timeout + 100
+    );
+}
+
+function throttled_fade_in(ele, timeout=fade_timeout) {
+    $(document).ready(function () {
+        $(ele).fadeIn(timeout);
+    });
+    setTimeout(
+        function()
+        {
+            return;
+        }, timeout);
+}
+
+function throttled_fade_out(ele, timeout=fade_timeout) {
+    $(document).ready(function () {
+        $(ele).fadeOut(timeout);
+    });
+    setTimeout(
+        function()
+        {
+            return;
+        }, timeout);
+}
+
+function throttled_fade_toggle(ele, timeout=fade_timeout) {
+    $(document).ready(function () {
+        $(ele).fadeToggle(timeout);
+    });
+    setTimeout(
+        function()
+        {
+            return;
+        }, timeout);
+}
